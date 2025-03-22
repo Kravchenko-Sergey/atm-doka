4994d1d8e654b4d3984188a0c6ee8c8fdc9eb9ba
Sergey Kravchenko <sergey.ose.pyatigorsk@gmail.com>
2025-03-22 20:44:55 +0300

    add evo pages

---
+++ src/app/evotor5i/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Evotor5i() {
+	return (
+		<div>
+			<Image src='/evo5i.jpg' width={500} height={500} alt='evo5i' />
+			<div className='my-4 text-xl'>Прошивка терминала</div>
+			<div className='text-sm text-gray-700 my-4'>
+				Чтобы прошить терминал нужно взять и прошить его
+			</div>
+			<div className='flex items-center gap-4'>
+				<div className='text-sm'>Актуальна версия прошивки: 4.9.11</div>
+				<Button>
+					<Link
+						href={
+							'https://drive.google.com/file/d/1zHNbojcHBEpuA6ZLaEWsBXmnnbTafb1g/view?usp=drive_link'
+						}
+					>
+						Скачать
+					</Link>
+				</Button>
+			</div>
+		</div>
+	)
+}
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem
+} from '@/components/ui/sidebar'
+
+const items = [
+	{
+		title: 'Эвотор 5i',
+		url: 'evotor5i'
+	},
+	{
+		title: 'Эвотор 7.3',
+		url: 'evotor73'
+	}
+]
+
+export function AppSidebar() {
+	return (
+		<Sidebar>
+			<SidebarHeader />
+			<SidebarContent>
+				<SidebarGroup />
+				<SidebarGroupLabel>Терминалы</SidebarGroupLabel>
+				<SidebarGroupContent>
+					<SidebarMenu>
+						{items.map((item) => (
+							<SidebarMenuItem key={item.title}>
+								<SidebarMenuButton asChild>
+									<a href={item.url}>
+										<span>{item.title}</span>
+									</a>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+						))}
+					</SidebarMenu>
+				</SidebarGroupContent>
+				<SidebarGroup />
+			</SidebarContent>
+		</Sidebar>
+	)
+}
