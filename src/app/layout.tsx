c549cb6a13d3b9acb62e5ed92cb6cc37bb7e2462
Sergey Kravchenko <sergey.ose.pyatigorsk@gmail.com>
2025-03-22 20:47:54 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 'use client'
 
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/app-sidebar'
-
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin']
-})
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin']
-})
 
 export default function RootLayout({
 	children
--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -46,6 +46,7 @@ export function AppSidebar() {
 				</SidebarGroupContent>
 				<SidebarGroup />
 			</SidebarContent>
+			<SidebarFooter />
 		</Sidebar>
 	)
 }
