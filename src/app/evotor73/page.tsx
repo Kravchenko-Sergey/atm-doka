a216c961d86938ff02377330072c977a959f7e25
Sergey Kravchenko <sergey.ose.pyatigorsk@gmail.com>
2025-03-23 16:23:45 +0300

    fix

--- src/app/evotor73/page.tsx
+++ src/app/evotor73/page.tsx
@@ -8,18 +8,22 @@ export default function evotor73() {
 	return (
 		<div>
 			<Image src='/evo73.jpg' width={500} height={500} alt='evo73' />
-			<div className='my-4 text-4xl'>Прошивка терминала</div>
+			<div className='my-4 text-2xl'>Прошивка терминала</div>
 			<div className='text-xl text-gray-700 my-4'>
 				Чтобы прошить терминал нужно взять и прошить его
 			</div>
 			<div className='flex items-center gap-4'>
+				<div>dffdfdsf</div>
+				<div>asdfasfdas</div>
+				<div>sdfasddsa</div>
 				<div className='text-xl'>Актуальная версия прошивки: 4.9.11</div>
 				<Button>
 					<Link
 						href={
-							'https://drive.google.com/file/d/1GfdNdQIm04HbXzrNA0fyVDik_z3q98Xk/view?usp=sharing'
+							'https://drive.google.com/file/d/1GfdNdQIm04HbXzrNA0fyVDik_z3q98Xk/view?usp=drive_link'
 						}
 						target='blank'
+						download
 					>
 						Скачать
 					</Link>
