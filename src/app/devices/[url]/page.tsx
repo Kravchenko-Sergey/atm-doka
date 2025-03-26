c0d3c512dd39215b3dc5ea2bf1a5656b765df930
Sergey Kravchenko <sergey.ose.pyatigorsk@gmail.com>
2025-03-26 21:23:14 +0300

    add dynamic routes for devices

---
+++ src/app/devices/[url]/page.tsx
@@ -0,0 +1,196 @@
+import { v4 } from 'uuid'
+import { badgeVariants } from '@/components/ui/badge'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const devices = [
+	{
+		id: v4(),
+		title: 'Эвотор 5i',
+		url: 'evotor5i',
+		content: (
+			<div>
+				<Image src='/evo5i.jpg' width={500} height={500} alt='evo5i' />
+				<h2 className='mt-4 mb-8 text-3xl'>Как прошить?</h2>
+				<ul className='list-disc text-xl flex flex-col gap-4'>
+					<li>Включите смарт-терминал и подключите его к интернету.</li>
+					<li>
+						Очистите файловую систему. Для этого перейдите в Обслуживание кассы
+						→ Эквайринг или Эвотор.PAY → Файловая система → Форматировать.
+						Нажмите Запрос на обновление.
+					</li>
+					<li>
+						Загрузите обновления платёжного модуля. Для этого перейдите в
+						Обслуживание кассы → Эквайринг или Эвотор.PAY → Прошивка. Нажмите
+						Запрос на обновление.
+					</li>
+					<li>
+						После обновления проверьте какая версия прошивки установлена, для
+						этого зайдите в раздел Настройки → Обслуживание кассы → Эквайринг
+						или Эвотор.PAY → Общая информация.
+					</li>
+					<li>
+						На данный момент актуальная версия для 520 и 521 - 1.8.2.1, для 522
+						- 3.1.2.1
+					</li>
+					<li>
+						<Link
+							href={
+								'https://drive.google.com/uc?export=download&id=1zHNbojcHBEpuA6ZLaEWsBXmnnbTafb1g'
+							}
+							target='_blank'
+							download
+							className={`${badgeVariants({ variant: 'outline' })} mr-2`}
+						>
+							Скачайте
+						</Link>
+						файл прошивки. На данный момент актуальная версия прошивки 4.9.11
+					</li>
+					<li>Подготовте флешку в формате FAT32</li>
+					<li>Поместите файл прошивки в корень флешки</li>
+					<li>Включите кассу</li>
+					<li>
+						Вставте флешку в кассу. Устройство автоматически начнёт прошивку. На
+						экране появится окно &apos;Обновление системы&apos;.
+					</li>
+					<li>Нажмите на &apos;Проверить и установить.&apos;</li>
+					<li>
+						Касса начнет расшифровывать файл с обновлением, это займет некоторое
+						время. После успешной установки касса сама перезагрузится. Подождите
+						еще 1-2 минуты и вытащите флешку.
+					</li>
+				</ul>
+			</div>
+		)
+	},
+	{
+		id: v4(),
+		title: 'Эвотор 7.2',
+		url: 'evotor72',
+		content: (
+			<div>
+				<Image src='/evo72.jpg' width={500} height={500} alt='evo72' />
+				<h2 className='mt-4 mb-8 text-3xl'>Как прошить?</h2>
+				<ul className='list-disc text-xl flex flex-col gap-4'>
+					<li>
+						<Link
+							href={
+								'https://drive.google.com/uc?export=download&id=1n08SYgy72qF2susGP8Cnmw5tbupIaXX5'
+							}
+							target='_blank'
+							download
+							className={`${badgeVariants({ variant: 'outline' })} mr-2`}
+						>
+							Скачайте
+						</Link>
+						файл прошивки. На данный момент актуальная версия прошивки 4.9.11
+					</li>
+					<li>Подготовте флешку в формате FAT32</li>
+					<li>Поместите файл прошивки в корень флешки</li>
+					<li>Включите кассу</li>
+					<li>
+						Вставте флешку в кассу. Устройство автоматически начнёт прошивку. На
+						экране появится окно &apos;Обновление системы&apos;
+					</li>
+					<li>Нажмите на &apos;Проверить и установить.&apos;</li>
+					<li>
+						Касса начнет расшифровывать файл с обновлением, это займет некоторое
+						время. После успешной установки касса сама перезагрузится. Подождите
+						еще 1-2 минуты и вытащите флешку.
+					</li>
+				</ul>
+			</div>
+		)
+	},
+	{
+		id: v4(),
+		title: 'Эвотор 7.3',
+		url: 'evotor73',
+		content: (
+			<div>
+				<Image src='/evo73.jpg' width={500} height={500} alt='evo73' />
+				<h2 className='mt-4 mb-8 text-3xl'>Как прошить?</h2>
+				<ul className='list-disc text-xl flex flex-col gap-4'>
+					<li>
+						<Link
+							href={
+								'https://drive.google.com/uc?export=download&id=1GfdNdQIm04HbXzrNA0fyVDik_z3q98Xk'
+							}
+							target='_blank'
+							download
+							className={`${badgeVariants({ variant: 'outline' })} mr-2`}
+						>
+							Скачайте
+						</Link>
+						файл прошивки. На данный момент актуальная версия прошивки 4.9.11
+					</li>
+					<li>Подготовте флешку в формате FAT32</li>
+					<li>Поместите файл прошивки в корень флешки</li>
+					<li>Включите кассу</li>
+					<li>
+						Вставте флешку в кассу. Устройство автоматически начнёт прошивку. На
+						экране появится окно &apos;Обновление системы&apos;.
+					</li>
+					<li>Нажмите на &apos;Проверить и установить.&apos;</li>
+					<li>
+						Касса начнет расшифровывать файл с обновлением, это займет некоторое
+						время. После успешной установки касса сама перезагрузится. Подождите
+						еще 1-2 минуты и вытащите флешку.
+					</li>
+				</ul>
+			</div>
+		)
+	},
+	{
+		id: v4(),
+		title: 'Эвотор 10',
+		url: 'evotor10',
+		content: (
+			<div>
+				<Image src='/evo10.jpg' width={500} height={500} alt='evo10' />
+				<h2 className='mt-4 mb-8 text-3xl'>Как прошить?</h2>
+				<ul className='list-disc text-xl flex flex-col gap-4'>
+					<li>
+						<Link
+							href={
+								'https://drive.google.com/uc?export=download&id=1BO5nAbUB9ob3fGe2XGn954FrJZLoIVe2'
+							}
+							target='_blank'
+							download
+							className={`${badgeVariants({ variant: 'outline' })} mr-2`}
+						>
+							Скачайте
+						</Link>
+						файл прошивки. На данный момент актуальная версия прошивки 4.9.11
+					</li>
+					<li>Подготовте флешку в формате FAT32</li>
+					<li>Поместите файл прошивки в корень флешки</li>
+					<li>Включите кассу</li>
+					<li>
+						Вставте флешку в кассу. Устройство автоматически начнёт прошивку. На
+						экране появится окно &apos;Обновление системы&apos;.
+					</li>
+					<li>Нажмите на &apos;Проверить и установить.&apos;</li>
+					<li>
+						Касса начнет расшифровывать файл с обновлением, это займет некоторое
+						время. После успешной установки касса сама перезагрузится. Подождите
+						еще 1-2 минуты и вытащите флешку.
+					</li>
+				</ul>
+			</div>
+		)
+	}
+]
+
+export async function generateStaticParams() {
+	return devices.map((device) => ({
+		url: device.id
+	}))
+}
+
+const DevicePage = ({ params }: { params: { url: string } }) => {
+	const device = devices.find((device) => device.url === params.url)
+	return device?.content
+}
+
+export default DevicePage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@
 import { v4 } from 'uuid'
 import { Card, CardContent } from '@/components/ui/card'
 
-//import { Input } from '@/components/ui/input'
-
 export default function Home() {
-	const items = [
+	const devices = [
 		{
 			id: v4(),
 			title: 'Эвотор 5i',
@@ -29,67 +27,83 @@ export default function Home() {
 		}
 	]
 
+	const messages = [
+		{
+			id: v4(),
+			title: 'Друг для друга',
+			description:
+				'Дока — это документация для POS-инженеров на понятном языке. Её пишет сообщество, чтобы помогать друг другу. Ваши знания и опыт важны. Делитесь ими, мы поможем.'
+		},
+		{
+			id: v4(),
+			title: 'Дока ищет партнёров',
+			description:
+				'Партнёрство с Докой полезно компании и сотрудникам. Поддерживая Доку, вы помогаете развивать русскоязычное сообщество POS-инженеров.'
+		}
+	]
+
+	const posts = [
+		{
+			id: v4(),
+			title: 'Здесь может быть ваша статья'
+		},
+		{
+			id: v4(),
+			title: 'Здесь может быть ваша статья'
+		},
+		{
+			id: v4(),
+			title: 'Здесь может быть ваша статья'
+		},
+		{
+			id: v4(),
+			title: 'Здесь может быть ваша статья'
+		},
+		{
+			id: v4(),
+			title: 'Здесь может быть ваша статья'
+		},
+		{
+			id: v4(),
+			title: 'Здесь может быть ваша статья'
+		}
+	]
+
 	return (
 		<>
 			<div className='mb-8 flex flex-col gap-4'>
-				{items.map((item) => (
-					<div key={item?.title}>
+				{devices.map((device) => (
+					<div key={device?.id}>
 						<div>
-							<a href={item?.url} className='hover:underline'>
-								<span className='text-xl'>{item?.title}</span>
+							<a href={`/devices/${device.url}`} className='hover:underline'>
+								<span className='text-xl'>{device?.title}</span>
 							</a>
 						</div>
 					</div>
 				))}
 			</div>
 			<div className='flex gap-8 flex-wrap lg:flex-nowrap'>
-				<div className='p-8 w-[50%] bg-gray-200 rounded-sm flex-auto'>
-					<div className='text-2xl'>Друг для друга</div>
-					<div className='my-8 text-xl'>
-						Дока — это документация для POS-инженеров на понятном языке. Её
-						пишет сообщество, чтобы помогать друг другу. Ваши знания и опыт
-						важны. Делитесь ими, мы поможем.
+				{messages.map((message) => (
+					<div
+						key={message.id}
+						className='p-8 w-[50%] bg-gray-200 rounded-sm flex-auto'
+					>
+						<div className='text-2xl'>{message.title}</div>
+						<div className='my-8 text-xl'>{message.description}</div>
 					</div>
-				</div>
-				<div className='p-8 w-[50%] border rounded-sm flex-auto'>
-					<div className='text-2xl'>Дока ищет партнёров</div>
-					<div className='my-8 text-xl'>
-						Партнёрство с Докой полезно компании и сотрудникам. Поддерживая
-						Доку, вы помогаете развивать русскоязычное сообщество POS-инженеров.
-					</div>
-				</div>
+				))}
 			</div>
 			<div className='py-8 flex gap-8 flex-wrap'>
-				<Card className='h-[560px] flex items-center justify-center flex-auto border-[#2e9aff]'>
-					<CardContent>
-						<p className='text-xl'>Здесь может быть ваша статья</p>
-					</CardContent>
-				</Card>
-				<Card className='h-[560px] flex items-center justify-center flex-auto border-[#41e747]'>
-					<CardContent>
-						<p className='text-xl'>Здесь может быть ваша статья</p>
-					</CardContent>
-				</Card>
-				<Card className='h-[560px] flex items-center justify-center flex-auto border-[#ff852f]'>
-					<CardContent>
-						<p className='text-xl'>Здесь может быть ваша статья</p>
-					</CardContent>
-				</Card>
-				<Card className='h-[560px] flex items-center justify-center flex-auto border-[#ffd829]'>
-					<CardContent>
-						<p className='text-xl'>Здесь может быть ваша статья</p>
-					</CardContent>
-				</Card>
-				<Card className='h-[560px] flex items-center justify-center flex-auto border-[#2e9aff]'>
-					<CardContent>
-						<p className='text-xl'>Здесь может быть ваша статья</p>
-					</CardContent>
-				</Card>
-				<Card className='h-[560px] flex items-center justify-center flex-auto border-[#12f3af]'>
-					<CardContent>
-						<p className='text-xl'>Здесь может быть ваша статья</p>
-					</CardContent>
-				</Card>
+				{posts.map((post) => (
+					<Card
+						key={post.id}
+						className='h-[560px] flex items-center justify-center flex-auto border-[#2e9aff]'
+					>
+						<CardContent>
+							<p className='text-xl'>{post.title}</p>
+						</CardContent>
+					</Card>
+				))}
 			</div>
 		</>
 	)
