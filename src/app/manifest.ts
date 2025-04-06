947006ab48f95a4834f487ffd3e1fc4d74f021f5
Sergey Kravchenko <sergey.ose.pyatigorsk@gmail.com>
2025-04-06 21:18:09 +0300

    change manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,8 +2,8 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
 	return {
-		name: 'АТМ-Дока',
-		short_name: 'Дока',
+		name: 'АТМ Дока',
+		short_name: 'АТМ Дока',
 		description: 'A Progressive Web App built with Next.js',
 		start_url: '/',
 		display: 'standalone',
