f8d48b9e2082a794c2c999668b92eda0b14912fd
Sergey Kravchenko <sergey.ose.pyatigorsk@gmail.com>
2025-05-24 23:59:45 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -99,21 +99,6 @@ export default function RootLayout({
 			<body
 				className={`min-h-screen w-full bg-white text-[#2C2C2C] dark:bg-[#292a2e] dark:text-white`}
 			>
-				<Script id='metrika-counter' strategy='afterInteractive'>
-					{`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();
-    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
- 
-    ym(102141824, "init", {
-          defer: true,
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true,
-          webvisor:true
-    });`}
-				</Script>
 				<Suspense fallback={<></>}>
 					<YandexMetrika />
 				</Suspense>
